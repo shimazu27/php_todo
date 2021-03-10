96397b0d14d67aebb23893c76e0e437dc49983ac
shimazu27 <vaigaku-tehutehu.doa@i.softban.jp>
2021-03-10 18:12:51 +0900

    day3

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
-if(isset($_POST['submit'])){$name = $_POST['name'];
+require('db_connect.php');
+if(isset($_POST['submit'])){
+  $name=$_POST['name'];
   $name=htmlspecialchars($name,ENT_QUOTES);
-require('db_connect.php');}
 
-$sql = 'INSERT INTO todos (name,task) VALUES (?,0)';
-$stmt = $dbh->prepare($sql);
+$sql='INSERT INTO todos (name,task) VALUES (?,0)';
+$stmt=$dbh->prepare($sql);
 
 $stmt->bindvalue(1,$name,PDO::PARAM_STR);
 $stmt->execute();
-$dbd = null;
 unset($name);
-
+}
 ?>
 
 <html>
@@ -21,25 +21,28 @@ unset($name);
 <body>
   <h1>Todoリスト</h1>
   <form action="index.php" method="post">
-    <ul>
-      <li><span>タスク名</span><input type="text" name="name">
-      <input type="submit" name="submit"></li>
-    </ul>
+    <p><label>名前<input type="text" name="name"></label></a>
+    <p><label>task<input type="text" name="task"></label></a>
+    <p><input type="submit" name="submit"></p>
   </form>
   <br>
   <table border="1">
     <tr>
       <th>名前</th>
-      <th>todo</th>
+      <th>task</th>
     </tr>
 
     <?php
-    $sql = 'SELECT id, name,todo FROM todos';
-    
-     while($task = $stmt->fetch()) {
+    $dbh=db_connect();
+    $sql='SELECT id, name,task FROM todos';
+    $stmt=$dbh->prepare($sql);
+    $stmt->execute();
+    $dbh=null;
+
+     while($todo = $stmt->fetch()) {
       echo "<tr>";
-      echo "<td>".$task["name"]."</td>";
-      echo "<td>".$task["todo"]."</td>";
+      echo "<td>".$task['name']."</td>";
+      echo "<td>".$task['task']."</td>";
 
       echo "<td>";
       echo "<form action='index.php' method='post'>";
